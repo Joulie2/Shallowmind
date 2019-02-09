519e22f9d0a91a245e3b69ef67577fbaf48d720c
Kantaja <kantaja98@gmail.com>
2019-02-09 17:16:57 +0000

    the real truth

--- example.py
+++ example.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-https://fbcdn-sphotos-e-a.hubatka.cz/hphotos-ak-prn1/44530_491807354174208_665546187_n.jpg
-=======
-print("example")
->>>>>>> parent of 3e81c1a... click
+print("harvey a scrub")
